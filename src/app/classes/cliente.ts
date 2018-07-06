313379d96fb0971ba5f5e6ab567f6fc617e26742
Miguel Cortegana <miguel.cortegana93@gmail.com>
2018-07-06 04:05:09 -0500

    Test Deploy

--- src/app/classes/cliente.ts
+++
@@ -1,7 +0,0 @@
-export class Cliente {
-  dni: string;
-  nom_clie: string;
-  correo: string;
-  telefono_f: string;
-  telefono_c: string;
-}
--- src/app/classes/funcion.ts
+++
@@ -1,8 +0,0 @@
-export class Funcion {
-  id_funcion: string;
-  nom_pelicula: string;
-  num_sala: string;
-  fecha: string;
-  inicio: string;
-  fin: string;
-}
